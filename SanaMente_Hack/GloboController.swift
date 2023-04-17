654cb9087164da3b782e36565e293026fec1cde3
paulinalh <98052784+rcallejap@users.noreply.github.com>
2023-04-16 22:16:24 -0600

    pantalla globo

--- SanaMente_Hack/GloboController.swift
+++ SanaMente_Hack/GloboController.swift
@@ -6,13 +6,55 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class GloboController: UIViewController {
+class GloboController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    
+    var scene = SCNScene(named: "art.scnassets/mainScene.scn")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.scene = scene
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+    
+           // Create a session configuration
+           let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+    
+           // Run the view's session
+           sceneView.session.run(configuration)
+        }
+    //
+       override func viewWillDisappear(_ animated: Bool) {
+          super.viewWillDisappear(animated)
+    
+           // Pause the view's session
+           sceneView.session.pause()
+       }
+    
+    func addAnimation(node: SCNNode){
+        let inhale = SCNAction.scale(to: 1.4, duration: 4.0)
+        let pause = SCNAction.wait(duration: 7.0)
+        let exhale = SCNAction.scale(to: 1.0, duration: 8.0)
+        
+        let bSequence = SCNAction.sequence([inhale, pause, exhale])
+        node.runAction(bSequence)
+    }
+    
+    
+    @IBAction func startButton(_ sender: Any) {
+        let containerNode = scene.rootNode.childNode(withName: "balloon reference", recursively: false)!
+        addAnimation(node: containerNode)
     }
     
 
